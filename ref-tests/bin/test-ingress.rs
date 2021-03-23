955d8937c87bfb1f275f4b598f9f5847e4de0b21
Victor Miraldo <victor.miraldo@dfinity.org>
2021-03-23 15:04:39 +0100

    Making a binary

---
+++ ref-tests/bin/test-ingress.rs
@@ -0,0 +1,7 @@
+use ic_agent::{Agent, AgentError, Identity, RequestId};
+use ic_utils::call::AsyncCall;
+use ic_utils::interfaces::ManagementCanister;
+
+fn main() {
+    println!("Ok, lets go!");
+}
